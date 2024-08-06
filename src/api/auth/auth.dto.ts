2bc2e9be300e24cb8c835b15f1a8bd6efdd93d48
sxlwrl <vadymkazak2019@gmail.com>
2024-08-06 14:52:20 +0300

    [add] register DTO

---
+++ src/api/auth/auth.dto.ts
@@ -0,0 +1,53 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
+/**
+ * DTO for patient registration
+ */
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 50)
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 50)
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 50)
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 10)
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 250)
+  password: string;
+
+  constructor({
+    username,
+    firstName,
+    lastName,
+    email,
+    password,
+  }: Record<string, string>) {
+    this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+/**
+ * DTO for patient login
+ */
+
+export class LoginDto {}
