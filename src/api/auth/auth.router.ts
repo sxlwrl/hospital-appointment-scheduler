6a1c71df056f3656810b3d776e55c34b586e0403
sxlwrl <vadymkazak2019@gmail.com>
2024-08-06 15:20:00 +0300

    [add] auth router

---
+++ src/api/auth/auth.router.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { Pool } from 'pg';
+import { AuthService } from './auth.service';
+import { PatientRepository } from '../patient/patient.repository';
+import { AuthController } from './auth.controller';
+
+export class AuthRouter {
+  public router: Router;
+  private readonly authService;
+  private readonly authController;
+
+  constructor(private readonly pool: Pool) {
+    this.router = Router();
+    this.authService = new AuthService(new PatientRepository(this.pool));
+    this.authController = new AuthController(this.authService);
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router.post('/auth/register', this.authController.register);
+    // this.router.post(
+    //     '/auth/login', this.authController.login,
+    // );
+  }
+}
