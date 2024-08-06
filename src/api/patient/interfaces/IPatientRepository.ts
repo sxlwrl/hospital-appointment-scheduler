2a8517ff777c4e6541b1eb77fad8558b154f0bb5
sxlwrl <vadymkazak2019@gmail.com>
2024-08-06 13:49:06 +0300

    [change] properties of methods of user search in the database by certain parameters were added

--- src/api/patient/interfaces/IPatientRepository.ts
+++ src/api/patient/interfaces/IPatientRepository.ts
@@ -2,9 +2,10 @@ import { CreatePatientDto, UpdatePatientDto } from '../patient.dto';
 import { Patient } from '../patient.model';
 
 export interface IPatientRepository {
-  findById(id: number): Promise<Patient | null>;
+  // findById(id: number): Promise<Patient | null>;
   findByUsername(username: string): Promise<Patient | null>;
+  findByEmail(email: string): Promise<Patient | null>;
   create(data: CreatePatientDto): Promise<Patient>;
-  update(data: UpdatePatientDto): Promise<Patient>;
-  delete(id: number): Promise<void>;
+  // update(data: UpdatePatientDto): Promise<Patient>;
+  // delete(id: number): Promise<void>;
 }
