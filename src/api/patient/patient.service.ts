83431dcc8526f5edd4129fba5e812eb8d0c8ce6e
sxlwrl <vadymkazak2019@gmail.com>
2024-08-22 17:26:33 +0300

    [add] patient service

---
+++ src/api/patient/patient.service.ts
@@ -0,0 +1,47 @@
+import { IPatientRepository } from './interfaces/IPatientRepository';
+import { UpdatePatientDto } from './patient.dto';
+import { join } from 'path';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+
+dotenv.config({ path: join(__dirname, '../../../.env') });
+
+/**
+ * Patient service
+ */
+
+export class PatientService {
+  constructor(private readonly patientRepository: IPatientRepository) {}
+
+  async findById(id: number) {
+    const patient = await this.patientRepository.findById(id);
+
+    if (!patient) {
+      throw new Error('Patient with described id doesnt exist');
+    }
+
+    return patient;
+  }
+
+  async findAll() {
+    return await this.patientRepository.findAll();
+  }
+
+  async updatePatient(id: number, data: UpdatePatientDto) {
+    const patient = await this.findById(id);
+
+    const hashedPassword = data.password
+      ? await bcrypt.hash(data.password, 10)
+      : patient.passwordHash;
+
+    return await this.patientRepository.update(id, {
+      ...data,
+      password: hashedPassword,
+    });
+  }
+
+  async deletePatient(id: number) {
+    await this.findById(id);
+    await this.patientRepository.delete(id);
+  }
+}
