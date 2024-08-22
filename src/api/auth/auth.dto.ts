7c705078e78318cdf27476ef6a4dea4ad5664709
sxlwrl <vadymkazak2019@gmail.com>
2024-08-22 17:07:44 +0300

    [change] register dto extends createPatient dto

--- src/api/auth/auth.dto.ts
+++ src/api/auth/auth.dto.ts
@@ -1,78 +1,22 @@
 import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+  UsernameValidation,
+  PasswordValidation,
+} from '../../utils/validationDecorators';
+import { CreatePatientDto } from '../patient/patient.dto';
 
 /**
  * DTO for patient registration
  */
 
-export class RegisterDto {
-  @IsNotEmpty({ message: 'Username cannot be empty' })
-  @IsString({ message: 'Username must be of string type' })
-  @MinLength(5, { message: 'Min length is 5' })
-  @MaxLength(50, { message: 'Max length is 50' })
-  username: string;
-
-  @IsNotEmpty({ message: 'First name cannot be empty' })
-  @IsString({ message: 'First name must be of string type' })
-  @MinLength(5, { message: 'Min length is 5' })
-  @MaxLength(50, { message: 'Max length is 50' })
-  firstName: string;
-
-  @IsNotEmpty({ message: 'Last name cannot be empty' })
-  @IsString({ message: 'Last name must be of string type' })
-  @MinLength(5, { message: 'Min length is 5' })
-  @MaxLength(50, { message: 'Max length is 50' })
-  lastName: string;
-
-  @IsEmail()
-  @IsNotEmpty({ message: 'Email cannot be empty' })
-  @IsString({ message: 'Email must be of string type' })
-  @MinLength(10, { message: 'Min length is 1' })
-  @MaxLength(100, { message: 'Max length is 100' })
-  email: string;
-
-  @IsNotEmpty({ message: 'Password cannot be empty' })
-  @IsString({ message: 'Password must be of string type' })
-  @MinLength(5, { message: 'Min length is 5' })
-  @MaxLength(250, { message: 'Max length is 250' })
-  password: string;
-
-  constructor({
-    username,
-    firstName,
-    lastName,
-    email,
-    password,
-  }: Record<string, string>) {
-    this.username = username;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-  }
-}
+export class RegisterDto extends CreatePatientDto {}
 
 /**
  * DTO for patient login
  */
 
 export class LoginDto {
-  @IsNotEmpty({ message: 'Username cannot be empty' })
-  @IsString({ message: 'Username must be of string type' })
-  @MinLength(5, { message: 'Min length is 5' })
-  @MaxLength(50, { message: 'Max length is 50' })
-  username: string;
-
-  @IsNotEmpty({ message: 'Password cannot be empty' })
-  @IsString({ message: 'Password must be of string type' })
-  @MinLength(5, { message: 'Min length is 5' })
-  @MaxLength(250, { message: 'Max length is 250' })
-  password: string;
+  @UsernameValidation() username: string;
+  @PasswordValidation() password: string;
 
   constructor({ username, password }: Record<string, string>) {
     this.username = username;
