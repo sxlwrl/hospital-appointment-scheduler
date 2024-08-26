2e2ee2107963ef39ea1cd812b0b598bfb325bbd8
sxlwrl <vadymkazak2019@gmail.com>
2024-08-27 01:09:08 +0300

    [change] findAll return type has been  to promise with an array

--- src/api/patient/interfaces/IPatientService.ts
+++ src/api/patient/interfaces/IPatientService.ts
@@ -1,13 +1,13 @@
+import { Patient } from '../patient.model';
+import { UpdatePatientDto } from '../patient.dto';
+
 /**
  * An interface of patient service
  */
 
-import { Patient } from '../patient.model';
-import { UpdatePatientDto } from '../patient.dto';
-
 export interface IPatientService {
   findById(id: number): Promise<Patient | null>;
-  findAll(): Promise<Patient>;
+  findAll(): Promise<Patient[]>;
   updatePatient(id: number, data: UpdatePatientDto): Promise<Patient>;
   deletePatient(id: number): Promise<void>;
 }
