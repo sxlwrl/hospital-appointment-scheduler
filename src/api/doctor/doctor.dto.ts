61d3b21eb7dfa7c4ed6999388fc96b820f070be0
sxlwrl <vadymkazak2019@gmail.com>
2024-08-31 19:52:13 +0300

    [add] doctor dto

---
+++ src/api/doctor/doctor.dto.ts
@@ -0,0 +1,56 @@
+import {
+  FirstNameValidation,
+  LastNameValidation,
+  IdValidation,
+} from '../../utils/validationDecorators';
+import { IsOptional } from 'class-validator';
+
+/**
+ * Base doctor DTO
+ */
+
+interface BaseDoctorDto {
+  firstName: string;
+  lastName: string;
+  specialization_id: number;
+}
+
+/**
+ * DTO for doctor creation
+ */
+
+export class CreateDoctorDto implements Required<BaseDoctorDto> {
+  @FirstNameValidation() firstName: string;
+  @LastNameValidation() lastName: string;
+  @IdValidation() specialization_id: number;
+
+  constructor({ firstName, lastName, specialization_id }) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.specialization_id = specialization_id;
+  }
+}
+
+/**
+ * DTO for updating doctors data
+ */
+
+export class UpdateDoctorDto implements Partial<BaseDoctorDto> {
+  @FirstNameValidation()
+  @IsOptional()
+  firstName: string;
+
+  @LastNameValidation()
+  @IsOptional()
+  lastName: string;
+
+  @IdValidation()
+  @IsOptional()
+  specialization_id: number;
+
+  constructor({ firstName, lastName, specialization_id }) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.specialization_id = specialization_id;
+  }
+}
