f9f649868327c20be6507a0bf1b29c6d87685232
sxlwrl <vadymkazak2019@gmail.com>
2024-08-31 20:54:02 +0300

    [add] doctor repository

---
+++ src/api/doctor/doctor.repository.ts
@@ -0,0 +1,86 @@
+import { Pool, QueryResult } from 'pg';
+import { Doctor } from './doctor.model';
+import { CreateDoctorDto, UpdateDoctorDto } from './doctor.dto';
+
+export class DoctorRepository {
+  constructor(private readonly pool: Pool) {}
+
+  async findById(id: number): Promise<Doctor | null> {
+    return this.findByData('id', id);
+  }
+
+  async findAll(): Promise<Doctor[]> {
+    const query = `SELECT * FROM doctors`;
+    const queryResult = await this.executeQuery(query);
+    return queryResult.rows.map(this.mapToDoctor);
+  }
+
+  async create(data: CreateDoctorDto): Promise<Doctor> {
+    const query = `INSERT INTO doctors (first_name, last_name, specialization_id) VALUES ($1, $2, $3) RETURNING *`;
+    const result = await this.executeQuery(query, [
+      data.firstName,
+      data.lastName,
+      data.specialization_id,
+    ]);
+    return this.mapToDoctor(result.rows[0]);
+  }
+
+  async update(id: number, data: UpdateDoctorDto): Promise<Doctor> {
+    const fields = [];
+    const values = [];
+
+    if (data.firstName) {
+      fields.push('first_name = $' + (fields.length + 1));
+      values.push(data.firstName);
+    }
+
+    if (data.lastName) {
+      fields.push('last_name = $' + (fields.length + 1));
+      values.push(data.lastName);
+    }
+
+    if (data.specialization_id) {
+      fields.push('specialization_id = $' + (fields.length + 1));
+      values.push(String(data.specialization_id));
+    }
+
+    const query = `UPDATE doctors SET ${fields.join(', ')} WHERE id = ${id} RETURNING *`;
+
+    const result = await this.executeQuery(query, values);
+    return this.mapToDoctor(result.rows[0]);
+  }
+
+  async delete(id: number): Promise<void> {
+    const query = 'DELETE FROM doctors WHERE id = $1';
+    await this.executeQuery(query, [id]);
+  }
+
+  private async findByData(field: string, value: any): Promise<Doctor | null> {
+    const query = `SELECT * FROM doctors WHERE ${field} = $1`;
+    const result = await this.executeQuery(query, [value]);
+    return result.rowCount ? this.mapToDoctor(result.rows[0]) : null;
+  }
+
+  private async executeQuery(
+    query: string,
+    values?: any[],
+  ): Promise<QueryResult> {
+    try {
+      return values
+        ? await this.pool.query(query, values)
+        : await this.pool.query(query);
+    } catch (error) {
+      throw new Error('Cannot execute query');
+    }
+  }
+
+  private mapToDoctor(row: any): Doctor {
+    if (!row) throw new Error('Cannot map to doctor');
+    return new Doctor(
+      row.id,
+      row.first_name,
+      row.last_name,
+      row.specialization_id,
+    );
+  }
+}
