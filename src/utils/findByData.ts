7f721f87a9406607739c6ac93a37efa226bc4fba
sxlwrl <vadymkazak2019@gmail.com>
2024-09-02 14:39:20 +0300

    [add] util function that find data in db by certain filed

---
+++ src/utils/findByData.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import { executeQuery } from './executeQuery';
+
+export const findByData = async <T>(
+  pool: Pool,
+  table: string,
+  field: string,
+  value: any,
+  mapFn: any,
+): Promise<T | null> => {
+  const query = `SELECT * FROM ${table} WHERE ${field} = $1`;
+  const result = await executeQuery(pool, query, [value]);
+  return result.rowCount ? mapFn(result.rows[0]) : null;
+};
