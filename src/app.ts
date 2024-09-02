5bf5b8bc4b7cff7038eabab4f423f94bd79ff3bf
sxlwrl <vadymkazak2019@gmail.com>
2024-09-02 13:53:35 +0300

    [change] added all created routes, changed db config

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,11 @@ import cookieParser from 'cookie-parser';
 import { join } from 'path';
 
 import { AuthRouter } from './api/auth/auth.router';
+import { PatientRouter } from './api/patient/patient.router';
+import { SpecializationRouter } from './api/specialization/specialization.router';
+import { DoctorRouter } from './api/doctor/doctor.router';
+import { AvailRouter } from './api/availability/avail.router';
+import { AppointmentRouter } from './api/appointment/appointment.router';
 
 dotenv.config({ path: join(__dirname, '../.env') });
 
@@ -17,16 +22,21 @@ app.use(express.urlencoded({ extended: false }));
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: 'localhost',
+  host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  database: 'hospital',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  database: 'postgres',
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({ message: err.message });
 });
 
-app.use('/api/v1', new AuthRouter(pool).router);
+app.use('/api/v1/auth', new AuthRouter(pool).router);
+app.use('/api/v1/patients', new PatientRouter(pool).router);
+app.use('/api/v1/specializations', new SpecializationRouter(pool).router);
+app.use('/api/v1/doctors', new DoctorRouter(pool).router);
+app.use('/api/v1/availabilities', new AvailRouter(pool).router);
+app.use('/api/v1/appointments', new AppointmentRouter(pool).router);
 
 export default app;
